d4a32d12443f21434ebb49262e1d64ac3e40df6b
Maize Cyber <maizecyber@pm.me>
2025-12-22 22:53:59 -0500

    Fixed validation errors in network.tf and instance

--- network.tf
+++ network.tf
@@ -1,12 +1,12 @@
 # Create the mynetwork network
 resource "google_compute_network" "mynetwork" {
-name = "mynetwork"
-# RESOURCE properties go here
-auto_create_subnetworks = "true"
+  name = "mynetwork"
+  # RESOURCE properties go here
+  auto_create_subnetworks = "true"
 }
 
-resource "google_compute_firewall" "allow_client_traffic" {
-  name = "all_client_traffic_ssh"
+resource "google_compute_firewall" "allow-client-traffic" {
+  name = "allow-client-traffic"
   # RESOURCE properties go here
   network = google_compute_network.mynetwork.self_link
   allow {
@@ -20,10 +20,10 @@ resource "google_compute_firewall" "allow_client_traffic" {
 }
 
 # Create the mynet-vm-1 instance
-module "mynet-vm-1" {
+module "minecraft-server-vm" {
   source           = "./instance"
   instance_name    = "mynet-vm-1"
   instance_zone    = "Zone"
-  machine_type     = "e2-micro"
+  instance_type     = "e2-micro"
   instance_network = google_compute_network.mynetwork.self_link
 }
