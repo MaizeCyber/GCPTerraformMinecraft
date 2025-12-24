a4cb70142aecd99fd9c0d1b3f542998918cda6fb
Maize Cyber <141431942+MaizeCyber@users.noreply.github.com>
2025-12-24 17:15:23 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,26 @@
 #!/bin/bash 
 sudo mkdir -p /home/minecraft
 sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/google-minecraft-disk
-mount /dev/disk/by-id/google-minecraft-disk /home/minecraft
+sudo mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft
 cd /home/minecraft
+sudo apt-get update
+sudo apt-get install -y default-jre-headless
+sudo apt-get install wget
 sudo wget https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar
 sudo java -Xmx1024M -Xms1024M -jar server.jar nogui
 sed -i '$c\eula=true' eula.txt
+sudo apt-get install -y screen
 sudo screen -d -m -S mcs java -Xmx1024M -Xms1024M -jar server.jar nogui
+sudo screen -r mcs
+# Source - https://stackoverflow.com/a
+# Posted by dogbane, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-24, License - CC BY-SA 3.0
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "0 0 * * * /home/minecraft/backup.sh" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
+
