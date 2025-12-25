cc440f8f35590d0bb23ddf8c33f2299ebe11fd07
Maize Cyber <141431942+MaizeCyber@users.noreply.github.com>
2025-12-24 22:05:57 -0500

    add service account permissions to write logs

--- network.tf
+++ network.tf
@@ -44,6 +44,11 @@ resource "google_service_account" "minecraft_sa" {
   display_name = "Minecraft Server Service Account"
 }
 
+resource "google_project_iam_member" "logging_writer" {
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.minecraft_sa.email}"
+}
+
 resource "google_storage_bucket_iam_member" "backup_writer" {
   bucket = google_storage_bucket.gcs_backup_bucket.name
   role   = "roles/storage.objectCreator"
