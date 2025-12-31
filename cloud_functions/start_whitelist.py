43a38183b77ef61404a2dfb84e845f5f062dace1
Maize Cyber <maizecyber@pm.me>
2025-12-31 15:18:31 -0500

    Started cloud function for whitelisting visitors

---
+++ cloud_functions/start_whitelist.py
@@ -0,0 +1,154 @@
+from __future__ import annotations
+import os
+import sys
+from typing import Any
+
+from google.api_core.extended_operation import ExtendedOperation
+from google.cloud import compute_v1
+
+from flask import abort
+from flask import request
+from flask import Flask
+
+server_ip = os.environ['SERVER_IP']
+project_id = os.environ['PROJECT_NAME']
+zone = os.environ['PROJECT_ZONE']
+instance_name = os.environ['INSTANCE_NAME']
+network_name = os.environ['NETWORK_NAME']
+
+def wait_for_extended_operation(
+    operation: ExtendedOperation, verbose_name: str = "operation", timeout: int = 300
+) -> Any:
+    """
+    Waits for the extended (long-running) operation to complete.
+
+    If the operation is successful, it will return its result.
+    If the operation ends with an error, an exception will be raised.
+    If there were any warnings during the execution of the operation
+    they will be printed to sys.stderr.
+
+    Args:
+        operation: a long-running operation you want to wait on.
+        verbose_name: (optional) a more verbose name of the operation,
+            used only during error and warning reporting.
+        timeout: how long (in seconds) to wait for operation to finish.
+            If None, wait indefinitely.
+
+    Returns:
+        Whatever the operation.result() returns.
+
+    Raises:
+        This method will raise the exception received from `operation.exception()`
+        or RuntimeError if there is no exception set, but there is an `error_code`
+        set for the `operation`.
+
+        In case of an operation taking longer than `timeout` seconds to complete,
+        a `concurrent.futures.TimeoutError` will be raised.
+    """
+    result = operation.result(timeout=timeout)
+
+    if operation.error_code:
+        print(
+            f"Error during {verbose_name}: [Code: {operation.error_code}]: {operation.error_message}",
+            file=sys.stderr,
+            flush=True,
+        )
+        print(f"Operation ID: {operation.name}", file=sys.stderr, flush=True)
+        raise operation.exception() or RuntimeError(operation.error_message)
+
+    if operation.warnings:
+        print(f"Warnings during {verbose_name}:\n", file=sys.stderr, flush=True)
+        for warning in operation.warnings:
+            print(f" - {warning.code}: {warning.message}", file=sys.stderr, flush=True)
+
+    return result
+
+def start_instance(project_id: str = project_id, zone: str = zone, instance_name: str = instance_name) -> None:
+    """
+    Starts a stopped Google Compute Engine instance (with unencrypted disks).
+    Args:
+        project_id: project ID or project number of the Cloud project your instance belongs to.
+        zone: name of the zone your instance belongs to.
+        instance_name: name of the instance your want to start.
+    """
+    instance_client = compute_v1.InstancesClient()
+
+    operation = instance_client.start(
+        project=project_id, zone=zone, instance=instance_name
+    )
+
+    wait_for_extended_operation(operation, "instance start")
+
+def create_firewall_rule(
+    project_id: str = project_id, firewall_rule_name: str, network: str = f"global/networks/{network_name}", visitor_ip: str
+) -> compute_v1.Firewall:
+    """
+    Creates a simple firewall rule allowing for incoming HTTP and HTTPS access from the entire Internet.
+
+    Args:
+        project_id: project ID or project number of the Cloud project you want to use.
+        firewall_rule_name: name of the rule that is created.
+        network: name of the network the rule will be applied to. Available name formats:
+            * https://www.googleapis.com/compute/v1/projects/{project_id}/global/networks/{network}
+            * projects/{project_id}/global/networks/{network}
+            * global/networks/{network}
+
+    Returns:
+        A Firewall object.
+        :param visitor_ip:
+    """
+    firewall_rule = compute_v1.Firewall()
+    firewall_rule.name = firewall_rule_name
+    firewall_rule.direction = "INGRESS"
+
+    allowed_ports = compute_v1.Allowed()
+    allowed_ports.I_p_protocol = "tcp"
+    allowed_ports.ports = ["25565"]
+
+    firewall_rule.allowed = [allowed_ports]
+    firewall_rule.source_ranges = [f"{visitor_ip}/32"]
+    firewall_rule.network = network
+    firewall_rule.description = f"Allowing TCP traffic on port 25565 from {visitor_ip}."
+
+    firewall_rule.target_tags = ["minecraft-server"]
+
+    # Note that the default value of priority for the firewall API is 1000.
+    # If you check the value of `firewall_rule.priority` at this point it
+    # will be equal to 0, however it is not treated as "set" by the library and thus
+    # the default will be applied to the new rule. If you want to create a rule that
+    # has priority == 0, you need to explicitly set it so:
+
+    firewall_rule.priority = 0
+
+    firewall_client = compute_v1.FirewallsClient()
+    operation = firewall_client.insert(
+        project=project_id, firewall_resource=firewall_rule
+    )
+
+    wait_for_extended_operation(operation, "firewall rule creation")
+
+    return firewall_client.get(project=project_id, firewall=firewall_rule_name)
+
+app = Flask(__name__)
+
+@app.route("/")
+def server_information():
+
+    if request.access_route:
+        visitor_ip = request.access_route[0]
+    else:
+        # Fallback for local development
+        visitor_ip = request.remote_addr
+
+    start_instance()
+
+    create_firewall_rule(firewall_rule_name=f"client-allow-{visitor_ip}", visitor_ip=visitor_ip)
+
+    print(f"Logged Visitor IP: {visitor_ip}")
+
+    return f"<p>Success! Server started if stopped. The IP of the server is {server_ip}</p>"
+
+if __name__ == "__main__":
+    # Cloud Run provides the PORT environment variable
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
--- instance/startup.sh
+++ instance/startup.sh
@@ -9,23 +9,41 @@ echo "Apt is free. Proceeding with installation."
 # Exit immediately if a command fails
 set -e
 
-#!/bin/bash
-set -e
+DEVICE_NAME = "google-minecraft-disk"
+MOUNT_PATH = "/home/minecraft"
+DISK_PATH= "/dev/disk/by-id/$DEVICE_NAME"
 
-sudo mkdir -p /home/minecraft
+if [ ! -d "$MOUNT_PATH" ]; then
+  sudo mkdir -p "$MOUNT_PATH"
+fi
 
-while [ ! -b "/dev/disk/by-id/google-minecraft-disk" ]; do
+while [ ! -b "$DISK_PATH" ]; do
   echo "Waiting for persistent disk to attach..."
   sleep 2
 done
 
-echo "Formatting disk"
-if [ -z "$(blkid /dev/disk/by-id/google-minecraft-disk)" ]; then
-  sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/google-minecraft-disk
+if mountpoint -q "$MOUNT_PATH"; then
+  echo "Disk is already mounted to $MOUNT_PATH. Skipping setup."
+else
+
+  # Format the disk only if it doesn't have a filesystem yet
+  if ! blkid "$DISK_PATH" > /dev/null; then
+    echo "New disk detected. Formatting with ext4..."
+    sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard "$DISK_PATH"
+  fi
+
+  echo "Mounting disk"
+  sudo mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft
+
+  # Ensure it mounts automatically on reboots
+  if ! grep -qs "$MOUNT_PATH" /etc/fstab; then
+    echo "$DISK_PATH $MOUNT_PATH ext4 discard,defaults,nofail 0 2" >> /etc/fstab
+  fi
+
+  echo "Persistent disk mounted successfully to $MOUNT_PATH"
+
 fi
 
-echo "Mounting disk"
-sudo mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft
 cd /home/minecraft
 
 echo "Running package updates"
@@ -34,33 +52,44 @@ sudo apt-get install wget
 
 sudo apt-get install -y wget ca-certificates
 
-echo "Downloading and installing java"
-wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
-sudo apt-get install -y ./jdk-21_linux-x64_bin.deb
+if ! command -v java &> /dev/null; then
+  echo "Downloading and installing java"
+  wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
+  sudo apt-get install -y ./jdk-21_linux-x64_bin.deb
+fi
 
 java -version
 
-echo "Downloading Minecraft server"
-sudo wget https://piston-data.mojang.com/v1/objects/64bb6d763bed0a9f1d632ec347938594144943ed/server.jar
-echo "eula=true" > eula.txt
+if [ ! -e "$MOUNT_PATH/server.jar" ]; then
+  echo "Downloading Minecraft server"
+  sudo wget https://piston-data.mojang.com/v1/objects/64bb6d763bed0a9f1d632ec347938594144943ed/server.jar
+  echo "eula=true" > eula.txt
+fi
+
 sudo apt-get install -y screen
 
 echo "Running server via screen..."
 sudo screen -d -m -S mcs java -Xmx1024M -Xms1024M -jar server.jar nogui
 
-echo "Downloading backup script"
-curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/backup_script_content" \
-  -H "Metadata-Flavor: Google" > /home/minecraft/backup.sh
+if [ ! -e "$MOUNT_PATH/backup.sh" ]; then
+  echo "Downloading backup script"
+  curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/backup_script_content" \
+    -H "Metadata-Flavor: Google" > /home/minecraft/backup.sh
+fi
 
 sudo chmod 755 /home/minecraft/backup.sh
 
-echo "Setting backup cron job"
-#write out current crontab
-crontab -l > mycron 2>/dev/null || touch mycron
-#echo new cron into cron file
-echo "0 0 * * * /home/minecraft/backup.sh" >> mycron
-#install new cron file
-crontab mycron
-rm mycron
+if crontab -l 2>/dev/null | grep -Fq "/home/minecraft/backup.sh"; then
+    echo "Cron job already exists. Skipping."
+else
+  echo "Setting backup cron job"
+  #write out current crontab
+  crontab -l > mycron 2>/dev/null || touch mycron
+  #echo new cron into cron file
+  echo "0 0 * * * /home/minecraft/backup.sh" >> mycron
+  #install new cron file
+  crontab mycron
+  rm mycron
+fi
 
 echo "Startup script complete"
