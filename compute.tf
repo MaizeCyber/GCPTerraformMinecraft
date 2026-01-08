2e5dc8075ff364a9aee234734c70653389f7f754
Maize Cyber <maizecyber@pm.me>
2026-01-07 23:07:52 -0500

    Fixed module name #build

--- compute.tf
+++ compute.tf
@@ -29,7 +29,7 @@ module "join-url-service" {
   run_name         = "join-url-1"
   run_region       = var.project_region
   sa_email         = google_service_account.cloud_run_sa.email
-  container_name   = "docker.io/maizecyber/MCServerJoinLink:gcp"
+  container_name   = "docker.io/maizecyber/mc-server-join:gcp"
   instance_name    = "minecraft-server-1"
   network_name     = "mynetwork"
   project_id       = var.project_name
