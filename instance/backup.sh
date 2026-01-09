a71b362e87afab3398a134dc514507b16cfe4672
Maize Cyber <maizecyber@pm.me>
2026-01-08 21:12:52 -0500

    Fixed backup folder logic #deploy

--- instance/backup.sh
+++ instance/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 screen -r mcs -X stuff '/save-all\n/save-off\n'
-/usr/bin/gcloud storage cp -R ${BASH_SOURCE%/*}/world gs://potato-swirl-landbridge-deaf/$(date "+%Y%m%d-%H%M%S")-world
+/usr/bin/gcloud storage cp -R ${BASH_SOURCE%/*}/world gs://potato-swirl-landbridge-deaf/$(date "+%Y%m%d-%H%M%S")-world/world
 screen -r mcs -X stuff '/save-on\n'
