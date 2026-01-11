b661d58ea42fd4631497409c6768edcfe2f2b904
Maize Cyber <maizecyber@pm.me>
2026-01-11 13:22:27 -0500

    Uniform bucket config #deploy

--- storage.tf
+++ storage.tf
@@ -25,6 +25,7 @@ data "archive_file" "function_file" {
 resource "google_storage_bucket" "function_bucket" {
   name     = "function-source-bucket-${var.project_id}"
   location = "US"
+  uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket_object" "function_object" {
