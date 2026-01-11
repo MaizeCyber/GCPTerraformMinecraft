6c3097885492e3c2124f58c0fb289c789ecf36d7
Maize Cyber <maizecyber@pm.me>
2026-01-10 19:13:31 -0500

    Updated version trigger #deploy

--- service_accounts.tf
+++ service_accounts.tf
@@ -9,6 +9,12 @@ resource "google_project_iam_member" "logging_writer" {
   member  = "serviceAccount:${google_service_account.minecraft_sa.email}"
 }
 
+resource "google_project_iam_member" "metrics_writer" {
+  project = var.project_id
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.minecraft_sa.email}"
+}
+
 resource "google_storage_bucket_iam_member" "backup_writer" {
   bucket = google_storage_bucket.gcs_backup_bucket.name
   role   = "roles/storage.objectAdmin"
